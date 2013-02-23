750173633c6ab986307a0a1fc40a09d9cc35b65a
Sergey Kishenin <sergey.kishenin@gmail.com>
2013-02-24 00:47:56 +0600

    tests are green :heart:

--- spec/namba/namba_spec.rb
+++ spec/namba/namba_spec.rb
@@ -14,14 +14,14 @@ describe Namba do
     Namba.configure do |config|
       config.username = "name"
       config.password = "secret"
-      config.locale = :kz
+      config.locale = :net
     end
 
     n = Namba.new()
 
     n.username.should == "name"    
     n.password.should == "secret"
-    n.locale.should == :kz
+    n.locale.should == :net
   end
 
   it "should set defaults when it is not set in configuration block" do
@@ -40,14 +40,14 @@ describe Namba do
       Namba.configure do |config|
         config.locale = :ru
       end 
-    }.to raise_error(Namba::ConfigurationError, "Available locales are only 'kg' and 'kz'")
+    }.to raise_error(Namba::ConfigurationError, "Available locales are only 'kg' and 'net'")
   end
   
   it "should raise Invalid Response Error when status code is not 200" do
     Namba.configure do |config|
       config.username = "name"
       config.password = "secret"
-      config.locale = :kz
+      config.locale = :net
     end
 
     n = Namba.new()
